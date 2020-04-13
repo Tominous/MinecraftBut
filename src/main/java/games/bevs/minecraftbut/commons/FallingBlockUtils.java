079bd68701afd86069d0491e2b68a2bce4668358
Tominous <18189490+Tominous@users.noreply.github.com>
2020-04-13 15:19:00 +0100

    Update FallingBlockUtils.java

--- src/main/java/games/bevs/minecraftbut/commons/FallingBlockUtils.java
+++ src/main/java/games/bevs/minecraftbut/commons/FallingBlockUtils.java
@@ -24,21 +24,21 @@ public class FallingBlockUtils {
 		}
 	}
 	
-	//if(fallingBlock.getBlockData().getMaterial() == XMaterial.ANVIL.parseMaterial()) 1.14
-	//if(fallingBlock.getBlockId() == 145) 1.8
+	if(fallingBlock.getBlockData().getMaterial() == XMaterial.ANVIL.parseMaterial()) 1.14
+	if(fallingBlock.getBlockId() == 145) 1.8
 	// will need optimizing
 	public static boolean fallingBlockMatch(FallingBlock fallingBlock, Material material) {
-//		try {
-//			Class<? extends FallingBlock> fallingclazz = fallingBlock.getClass();
-//			Method method = fallingclazz.getDeclaredMethod("getBlockData");
-//			Object blockData = method.invoke(fallingBlock);
-//			Class<? extends Object> blockDataClazz = blockData.getClass();
-//			Method method2 = blockDataClazz.getDeclaredMethod("getMaterial");
-//			Material fallingMaterial = (Material) method2.invoke(blockData);
-//			return fallingMaterial == material;
-//		} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-//			e.printStackTrace();
+		try {
+			Class<? extends FallingBlock> fallingclazz = fallingBlock.getClass();
+			Method method = fallingclazz.getDeclaredMethod("getBlockData");
+			Object blockData = method.invoke(fallingBlock);
+			Class<? extends Object> blockDataClazz = blockData.getClass();
+			Method method2 = blockDataClazz.getDeclaredMethod("getMaterial");
+			Material fallingMaterial = (Material) method2.invoke(blockData);
+			return fallingMaterial == material;
+		} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
 			return fallingBlock.getBlockId() == material.getId();
-//		}
+		}
 	}
 }
